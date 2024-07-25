f948c9283e0fa47bc8553b156776c48e74b6e544
HedvigS <hedvig.skirgard@gmail.com>
2024-07-25 15:37:00 +0200

    Update 02b_get_grambank.R

--- code/02b_get_grambank.R
+++ code/02b_get_grambank.R
@@ -11,16 +11,27 @@ SH.misc::fetch_lines("https://github.com/HedvigS/R_grambank_cookbook/raw/main/fu
 source("functions/make_binary_ValueTable.R")
 source("functions/reduce_ValueTable_to_unique_glottocodes.R")
 
+
 GB_LanguageTable <- read_csv("../../../../Grambank 2.0/cldf/languages.csv", show_col_types = FALSE)  
   
 GB_ValueTable <- read_csv("../../../../Grambank 2.0/cldf/values.csv", show_col_types = FALSE) %>% 
     make_binary_ValueTable(keep_multistate = F, keep_raw_binary = T) %>% 
   reduce_ValueTable_to_unique_glottocodes(LanguageTable = GB_LanguageTable, merge_dialects = T, method = "combine_random", replace_missing_language_level_ID = T, treat_question_mark_as_missing = T)
 
+GB_ValueTable %>% 
+  write_tsv("data/processed_data/Grambank_2.0_ValueTable.tsv", na = "", quote = "all")
+
 Glottolog_LanguageTable <- read_tsv("data/processed_data/glottolog_5.0_languages.tsv", show_col_types = F) %>% 
   filter(Family_ID == "indo1319")  %>% 
-  dplyr::select(Glottocode)
+  dplyr::select(Glottocode, Longitude, Latitude, Macroarea, Name, aes, med, Is_Isolate, classification, subclassification, Family_ID)
   
+GB_ValueTable %>% 
+  dplyr::select(Glottocode) %>% 
+  inner_join(Glottolog_LanguageTable, by = "Glottocode") %>%
+  distinct() %>% 
+  write_tsv("data/processed_data/GB_v2.0_LanguageTable.tsv")
+
+#props
 n_max <- GB_ValueTable$Parameter_ID %>% unique() %>% length()
 
 GB_props <- GB_ValueTable %>% 
