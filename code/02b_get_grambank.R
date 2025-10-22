18f8edde13c3f915046be209dfd86aaee925bbb7
HedvigS <hedvig.skirgard@gmail.com>
2025-10-22 20:58:37 +0200

    Update 02b_get_grambank.R

--- code/02b_get_grambank.R
+++ code/02b_get_grambank.R
@@ -1,27 +1,22 @@
 source("01_requirements.R")
 
-if(!file.exists("functions/make_binary_ValueTable.R")){
-SH.misc::fetch_lines("https://github.com/HedvigS/R_grambank_cookbook/raw/main/functions/make_binary_ValueTable.R", out_dir = "functions", add_date_fetched = T)
-}
-
-if(!file.exists("functions/reduce_ValueTable_to_unique_glottocodes.R")){
-SH.misc::fetch_lines("https://github.com/HedvigS/R_grambank_cookbook/raw/main/functions/reduce_ValueTable_to_unique_glottocodes.R", out_dir = "functions", add_date_fetched = T)
-}
-
-source("functions/make_binary_ValueTable.R")
-source("functions/reduce_ValueTable_to_unique_glottocodes.R")
-
-
-GB_LanguageTable <- read_csv("../../../../Grambank 2.0/cldf/languages.csv", show_col_types = FALSE)  
+GB_LanguageTable <- read_csv("../../../../grambank-v2.0rc2 2/cldf/languages.csv", show_col_types = FALSE)  
   
-GB_ValueTable <- read_csv("../../../../Grambank 2.0/cldf/values.csv", show_col_types = FALSE) %>% 
-    make_binary_ValueTable(keep_multistate = F, keep_raw_binary = T) %>% 
-  reduce_ValueTable_to_unique_glottocodes(LanguageTable = GB_LanguageTable, merge_dialects = T, method = "combine_random", replace_missing_language_level_ID = T, treat_question_mark_as_missing = T)
+GB_ValueTable <- read_csv("../../../../grambank-v2.0rc2 2/cldf/values.csv", show_col_types = FALSE) %>% 
+    rgrambank::make_binary_ValueTable(keep_multistate = FALSE, keep_native_binary = TRUE) %>% 
+  rgrambank::reduce_ValueTable_to_unique_glottocodes(LanguageTable = GB_LanguageTable, merge_dialects = T, method = "combine_random", replace_missing_language_level_ID = T)
 
 GB_ValueTable %>% 
-  write_tsv("data/processed_data/Grambank_2.0_ValueTable.tsv", na = "", quote = "all")
+  write_tsv("output/processed_data/Grambank_2.0_ValueTable.tsv", na = "", quote = "all")
 
-Glottolog_LanguageTable <- read_tsv("data/processed_data/glottolog_5.0_languages.tsv", show_col_types = F) %>% 
+
+GB_ParameterTable <- read_csv("../../../../grambank-v2.0rc2 2/cldf/parameters.csv", show_col_types = FALSE) %>% 
+  rgrambank::make_binary_ParameterTable(keep_multi_state_features = FALSE)
+
+GB_ParameterTable %>% 
+  write_tsv("output/processed_data/Grambank_2.0_ParameterTable.tsv", na = "", quote = "all")
+
+Glottolog_LanguageTable <- read_tsv("output/processed_data/glottolog_5.0_languages.tsv", show_col_types = F) %>% 
   filter(Family_ID == "indo1319")  %>% 
   dplyr::select(Glottocode, Longitude, Latitude, Macroarea, Name, aes, med, Is_Isolate, classification, subclassification, Family_ID)
   
@@ -29,7 +24,7 @@ GB_ValueTable %>%
   dplyr::select(Glottocode) %>% 
   inner_join(Glottolog_LanguageTable, by = "Glottocode") %>%
   distinct() %>% 
-  write_tsv("data/processed_data/GB_v2.0_LanguageTable.tsv")
+  write_tsv("output/processed_data/GB_v2.0_LanguageTable.tsv")
 
 #props
 n_max <- GB_ValueTable$Parameter_ID %>% unique() %>% length()
@@ -41,4 +36,4 @@ GB_props <- GB_ValueTable %>%
   mutate(prop = n/n_max)
   
 GB_props %>% 
-  write_tsv("data/processed_data/GB_v2.0_prop.tsv")
+  write_tsv("output/processed_data/GB_v2.0_prop.tsv")
