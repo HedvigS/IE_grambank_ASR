f7c740a6b4a1238a438f24c28d49f943af3efdc4
HedvigS <hedvig.skirgard@gmail.com>
2025-10-22 20:58:23 +0200

    update get glottolog script

--- code/02a_get_glottolog.R
+++ code/02a_get_glottolog.R
@@ -2,7 +2,7 @@
 
 source("01_requirements.R")
 
-if(!file.exists("data/processed_data/glottolog_5.0_languages.tsv")){
+if(!file.exists("output/processed_data/glottolog_5.0_languages.tsv")){
 
 # fetching Glottolog v5.0 from Zenodo using rcldf (requires internet)
 glottolog_rcldf_obj <- rcldf::cldf("https://zenodo.org/records/10804582/files/glottolog/glottolog-cldf-v5.0.zip", load_bib = F)
@@ -13,11 +13,11 @@ ValueTable_wide <- glottolog_rcldf_obj$tables$ValueTable %>%
 glottolog_rcldf_obj$tables$LanguageTable %>% 
   dplyr::rename(Language_level_ID = Language_ID, Language_ID = ID) %>% 
   full_join(ValueTable_wide, by = "Language_ID") %>% 
-  write_tsv("data/processed_data/glottolog_5.0_languages.tsv")
+  write_tsv("output/processed_data/glottolog_5.0_languages.tsv")
 }
 
-if(!file.exists("data/processed_data/glottolog_5.0_IE_tree.tree")){
-Glottolog_LanguageTable <- read_tsv("data/processed_data/glottolog_5.0_languages.tsv", show_col_types = F) %>% 
+if(!file.exists("output/processed_data/glottolog_5.0_IE_tree.tree")){
+Glottolog_LanguageTable <- read_tsv("output/processed_data/glottolog_5.0_languages.tsv", show_col_types = F) %>% 
   tibble::column_to_rownames("Language_ID")
 
 tree_IE_full <- ape::read.tree(text = Glottolog_LanguageTable["indo1319",]$subclassification)
@@ -25,5 +25,5 @@ tree_IE_full <- ape::read.tree(text = Glottolog_LanguageTable["indo1319",]$subcl
 tree_IE_full <- ape::compute.brlen(tree_IE_full, method = "grafen") %>% ladderize()
 
 tree_IE_full %>% 
-  ape::write.tree("data/processed_data/glottolog_5.0_IE_tree.tree")
+  ape::write.tree("output/processed_data/glottolog_5.0_IE_tree.tree")
 }
